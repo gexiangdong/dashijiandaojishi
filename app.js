ced152356a6410469c939a3619591c2e2492aae8
gexiangdong <gexiangdong@users.noreply.github.com>
2018-01-04 15:26:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 //app.js
 App({
-  onLaunch: function () {
-
-  },
+  onLaunch: function (options) {
+    if (options) {
+      try {
+        if (options.referrerInfo) {
+          var newEvents = options.referrerInfo.extraData
+          var myEvents = wx.getStorageSync('myevents') || []
+          var exists = false
+          for (var i = 0; i < myEvents.length; i++) {
+            for (var j = 0; i < newEvents.length; j++) {
+              if (newEvents[j].id == myEvents[i].id) {
+                exists = true
+                break
+              }
+            }
+            if (!exists) {
+              newEvents.push(myEvents[i])
+            }
+          }
+          getApp().globalData.myEvents = newEvents
+          wx.setStorageSync('myevents', newEvents)
+        }
+      } catch (ex) {
+        console.log(ex)
+      }
+    }
+  }
   globalData:{
     myEvents: null
   }
