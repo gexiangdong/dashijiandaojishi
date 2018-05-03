2b142bc8c1831a6e70387a24cba8e3f82ced2448
gexiangdong <gexiangdong@gmail.com>
2018-05-03 22:43:04 +0800

    update

--- server/app.js
+++ server/app.js
@@ -1,19 +1,52 @@
-const Koa = require('koa')
-const app = new Koa()
-const debug = require('debug')('koa-weapp-demo')
-const response = require('./middlewares/response')
-const bodyParser = require('koa-bodyparser')
 const config = require('./config')
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var rp = require('request-promise');
 
-// 使用响应处理中间件
-app.use(response)
+var app = express();
+app.use(bodyParser.json());
+// app.use(bodyParser.json({ limit: '1mb' }));  //body-parser 解析json格式数据
+// app.use(bodyParser.urlencoded({            //此项必须在 bodyParser.json 下面,为参数编码
+//   extended: true
+// }));
 
-// 解析请求体
-app.use(bodyParser())
+app.get('/', function (req, res) {
+  var data;
+  rp('https://dev.clschina.com/')
+    .then(function (respBody) {
+      data = respBody;
+      console.log(respBody) // 打印google首页
+      console.log('get / .... data' + data)
+      res.end('hello ' + data);
 
-// 引入路由分发
-const router = require('./routes')
-app.use(router.routes())
+    }).catch(function (err) {
+      console.log('error,', err)
+    })
+})
 
-// 启动程序，监听端口
-app.listen(config.port, () => debug(`listening on port ${config.port}`))
+app.post('/token', function (req, res) {
+  var code = req.body.code
+  console.log('/token....code=' + code)
+  rp('https://api.weixin.qq.com/sns/jscode2session?appid=' + config.appId + '&secret=' + config.appSecret + '&js_code=' + code + '&grant_type=authorization_code')
+    .then(function (body) {
+      console.log('response from weixin: ' + body)
+      var result = JSON.parse(body);
+      if (result['openid']) {
+        var openId = result['openid']
+        var sessionKey = result['session_key']
+        res.end(openId)
+      } else {
+        res.end('error:' + result['errcode'])
+      }
+    }).catch(function (err) {
+      console.log('error while get openid', err)
+      res.end('error ' + err);
+    })
+  
+})
+
+var server = app.listen(config.port, function () {
+    console.log("listening on port: %s", config.port)
+})
+
